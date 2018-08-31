cda8a35107caa4ee9940eb627c018da08ec56e5e
Liu Jiasheng <jiashengliu@foxmail.com>
2018-08-31 23:12:52 +0800

    LeetCode exam1
    for LeetCode exam1

---
+++ LeetCode/exam1/sum_for_two_num.py
@@ -0,0 +1,33 @@
+
+"""
+给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
+
+示例:
+给定 nums = [2, 7, 11, 15], target = 9
+因为 nums[0] + nums[1] = 2 + 7 = 9
+所以返回 [0, 1]
+"""
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+def main():
+
+    numbers = [2, 7, 11, 15]
+    target = 9
+
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            if numbers[i]+numbers[j] == target:
+                print(i,j)
+
+    print("test")
+
+
+if __name__ == "__main__":
+    main()
